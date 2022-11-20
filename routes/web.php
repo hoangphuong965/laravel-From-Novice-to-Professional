fc328820fef07fbed216d244653aef94393f9a2a
Phuong Hoang <you@example.com>
2022-11-20 16:15:39 +0700

    resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +24,9 @@ Route::get("/", WelcomeController::class);
 Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 Route::get("/contacts/{id}/{name}", [ContactController::class, 'show'])->name('contacts.show');
+
+Route::resource('/companies', CompanyController::class);
+Route::resources([
+    '/tags' => TagController::class,
+    '/tasks' => TaskController::class
+]);
